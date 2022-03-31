4a304f4900d0ae53d96b85494b08dcceb0d29f30
istephanos <96304065+istephanos@users.noreply.github.com>
2022-03-31 23:21:17 +0300

    Add files via upload

---
+++ Masina.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarieModele
+{
+    internal class Masina
+    {
+        //constante
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+
+        private const int ID = 0;
+        private const int FIRMA = 1;
+        private const int MODEL = 2;
+        private const int AN_FABRICATIE = 3;
+        private const int CULOARE = 4;
+        private const int DISPONIBILITATE =5;
+
+        //proprietati auto-implemented
+
+        private int idMasina;//identificator unic 
+        private string firma; //ex:Audi
+        private string model;//A4
+        private string an_fabricatie;
+        private string culoare;
+        private bool disponibilitate;
+
+
+        //constructor implicit
+        public Masina()
+        {
+            disponibilitate=false;
+            firma = model = an_fabricatie = culoare = string.Empty;
+        }
+
+        //constructor cu parametri
+        public Masina(int _id, string _firma, string _model, string _an_fabricatie, string _culoare)
+        {
+            idMasina = _id;
+            firma = _firma;
+            model = _model;
+            an_fabricatie = _an_fabricatie;
+            culoare = _culoare;
+            disponibilitate = true;
+        }
+
+        public Masina(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+            idMasina = Convert.ToInt32(dateFisier[ID]);
+            firma = dateFisier[FIRMA];
+            model = dateFisier[MODEL];
+            an_fabricatie = dateFisier[AN_FABRICATIE];
+            culoare = dateFisier[CULOARE];
+            //disponibilitate = dateFisier[DISPONIBILITATE];
+        }
+        public string ConversieLaSir_PentruFisier()
+        {
+            string obiectMasinaPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}",
+                SEPARATOR_PRINCIPAL_FISIER,
+                idMasina.ToString(),
+                (firma ?? " NECUNOSCUT "),
+                (model ?? " NECUNOSCUT "),
+                (an_fabricatie ?? " NECUNOSCUT "),
+                (culoare ?? "NECUNOSCUT"),
+                (disponibilitate , "NECUNOSCUT"));
+
+            return obiectMasinaPentruFisier;
+        }
+
+        public int GetIdMasina()
+        {
+            return idMasina;
+        }
+
+        public string GetFirma()
+        {
+            return firma;
+        }
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public string GetAnFabricatie()
+        {
+            return an_fabricatie;
+        }
+
+        public string GetCuloare()
+        {
+            return culoare;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Configuration;
+using LibrarieModele;
+using StocareDateClienti;
+
+namespace EvidentaClienti
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            AdministrareClienti_FisierText adminClienti = new AdministrareClienti_FisierText(numeFisier);
+            int nrClienti = 0;
+            string optiune;
+            do
+            {
+                Console.WriteLine("C.citire date de la tastatura");
+                Console.WriteLine("F. Afisare clienti din fisier");
+                Console.WriteLine("S. Salvare client in fisier");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "C":
+                        int i= 5;
+                        string cnp, nume, prenume;
+                        Console.WriteLine("Dati cnp-ul clientului: ");
+                        cnp = Console.ReadLine();
+                        while (cnp.Length != 13 && i!=0)
+                        {
+                            Console.WriteLine("CNP invalid-incercati din nou");
+                            cnp = Console.ReadLine();
+                            i--;
+                            if (i == 0)
+                            {
+                                Console.WriteLine("ne pare rau, dar ati introdus de prea multe ori CNP-ul gresit!");
+                                return;
+                            }
+                        }
+                        Console.WriteLine("Dati numele clientului: ");
+                        nume = Console.ReadLine();
+                        Console.WriteLine("Dati prenumele clientului: ");
+                        prenume = Console.ReadLine();
+                        Client client1 = new Client(nrClienti, cnp, nume, prenume);
+                        adminClienti.AddClient(client1);
+                        break;
+                    case "F":
+                        Client[] clienti = adminClienti.GetClienti(out nrClienti);
+                        AfisareClienti(clienti, nrClienti);
+                        break;
+                    case "S":
+                        int idClient = nrClienti + 1;
+                        nrClienti++;
+                        Client client = new Client(idClient, "5010729330215", "Ioana", "Radu");
+                        //adaugare client in fisier
+                        adminClienti.AddClient(client);
+                        break;
+                    case "L":
+                        string nume1, prenume1;
+                        Console.WriteLine("Dati numele clientului cautat: ");
+                        nume1 = Console.ReadLine();
+                        Console.WriteLine("Dati prenumele clientului cautat: ");
+                        prenume1 = Console.ReadLine();
+                        Console.WriteLine((adminClienti.GetClient(nume1, prenume1)).ToString());
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+        public static void AfisareClienti(Client[] clienti, int nrClienti)
+        {
+            Console.WriteLine("Clientii sunt:");
+            for (int contor = 0; contor < nrClienti; contor++)
+            {
+                string infoClient = string.Format("Clientul cu id-ul #{0} are numele: {1} {2} si CNP-ul {3} ",
+                   clienti[contor].GetIdClient(),
+                   clienti[contor].GetNume() ?? " NECUNOSCUT ",
+                   clienti[contor].GetPrenume() ?? " NECUNOSCUT ",
+                   clienti[contor].GetCNP() ?? " NECUNOSCUT ");
+
+                Console.WriteLine(infoClient);
+            }
+
+        }
+    }
+}
